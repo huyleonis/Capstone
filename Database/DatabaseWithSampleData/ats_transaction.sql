b783d57679f9cf23121b326c3ee62efeaba82d50
baotinpham <tin.bambo1112@gmail.com>
2017-10-05 21:34:48 +0700

    update db
    Change 1 số column trong các bảng account, beacon and transaction

--- Database/DatabaseWithSampleData/ats_transaction.sql
+++ Database/DatabaseWithSampleData/ats_transaction.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `transaction`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `transaction` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` varchar(20) NOT NULL ,
   `username_id` int(11) NOT NULL,
   `station_id` int(11) NOT NULL,
   `date_time` datetime NOT NULL,
