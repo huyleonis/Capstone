a21578543f2a0b80c548b4fe86fb71281b0a08ee
kaisonee <dovietson2000@gmail.com>
2017-10-11 09:42:18 +0700

    hoan thanh view transaction detail

---
+++ ATSApplication/app/src/main/java/com/example/hp/atsapplication/DetailTransactionActivity.java
@@ -0,0 +1,14 @@
+package com.example.hp.atsapplication;
+
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.hp.atsapplication.utils.RequestServer;
+
+/**
+ * Created by dovie on 10/11/2017.
+ */
+
+public class DetailTransactionActivity extends AppCompatActivity implements RequestServer.RequestResult {
+
+
+}
