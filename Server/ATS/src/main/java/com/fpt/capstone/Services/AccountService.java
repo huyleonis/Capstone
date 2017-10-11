9d7d7c1df0f843744c0f7238bd5b0dd27990ffd2
kaisonee <dovietson2000@gmail.com>
2017-10-11 20:57:45 +0700

    hoan thanh update account balance va cap nhat lai trang thai cua transaction

--- Server/ATS/src/main/java/com/fpt/capstone/Services/AccountService.java
+++ Server/ATS/src/main/java/com/fpt/capstone/Services/AccountService.java
@@ -1,5 +1,12 @@
 package com.fpt.capstone.Services;
 
+import com.fpt.capstone.Dtos.Account;
+
 public interface AccountService {
 
+    boolean checkAccountBalance(double priceToBeChecked, int accountId);
+
+    int updateAccountBalance(double amountToBeDeducted, int accountId);
+
+    Account getAccountById(int id);
 }
--- Server/ATS/src/main/java/com/fpt/capstone/Services/AccountServiceImpl.java
+++ Server/ATS/src/main/java/com/fpt/capstone/Services/AccountServiceImpl.java
@@ -1,8 +1,44 @@
 package com.fpt.capstone.Services;
 
+import com.fpt.capstone.Dtos.Account;
+import com.fpt.capstone.Repositories.AccountRepos;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountServiceImpl implements AccountService {
 
+    @Autowired
+    private AccountRepos accountRepos;
+
+    @Override
+    public boolean checkAccountBalance(double priceToBeChecked, int accountId) {
+        Account dto = Account.convertFromEntity(accountRepos.findAccountById(accountId));
+
+        if ((dto.getBalance() - priceToBeChecked) >= 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public int updateAccountBalance(double amountToBeDeducted, int accountId) {
+        int isSuccessful = 0;
+
+        Account dto = Account.convertFromEntity(accountRepos.findAccountById(accountId));
+
+        if ((dto.getBalance() - amountToBeDeducted) >= 0) {
+            isSuccessful = accountRepos.updateAccountBalance(dto.getBalance() - amountToBeDeducted, accountId);
+            return isSuccessful;
+        }
+
+        return isSuccessful;
+    }
+
+    @Override
+    public Account getAccountById(int id) {
+
+        return Account.convertFromEntity(accountRepos.findAccountById(id));
+    }
 }
