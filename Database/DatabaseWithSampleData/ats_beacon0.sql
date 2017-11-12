e52e9ea4575e128bb64bfbf13c0c107d1f039b36
huylvq <huy2896@gmail.com>
2017-11-13 00:20:02 +0700

    cập nhật db theo camelcase

---
+++ Database/DatabaseWithSampleData/ats_beacon0.sql
@@ -0,0 +1,61 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: ats
+-- ------------------------------------------------------
+-- Server version	5.7.17-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `beacon`
+--
+
+DROP TABLE IF EXISTS `beacon`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `beacon` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(50) NOT NULL,
+  `major` int(11) NOT NULL,
+  `minor` int(11) NOT NULL,
+  `stationId` int(11) NOT NULL,
+  `laneId` int(11) DEFAULT NULL,
+  `type` tinyint(1) NOT NULL,
+  `isActive` bit(1) NOT NULL DEFAULT b'1',
+  PRIMARY KEY (`id`),
+  KEY `fk_beacon_station_idx` (`stationId`),
+  KEY `fk_beacon_lane_idx` (`laneId`),
+  CONSTRAINT `fk_beacon_lane` FOREIGN KEY (`laneId`) REFERENCES `lane` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_beacon_station` FOREIGN KEY (`stationId`) REFERENCES `station` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `beacon`
+--
+
+LOCK TABLES `beacon` WRITE;
+/*!40000 ALTER TABLE `beacon` DISABLE KEYS */;
+INSERT INTO `beacon` VALUES (1,'B9407F30-F5F8-466E-AFF9-25556B57FE6D',36857,31381,1,NULL,0,''),(2,'B9407F30-F5F8-466E-AFF9-25556B57FE6D',39748,38452,1,1,1,''),(3,'B9407F30-F5F8-466E-AFF9-25556B57FE6D',24100,4804,1,2,1,'');
+/*!40000 ALTER TABLE `beacon` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-11-13  0:04:47
